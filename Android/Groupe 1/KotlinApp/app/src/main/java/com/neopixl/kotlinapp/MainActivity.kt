451aaa6f2e4341432042fecc76482c180fe7dcea
Yvan Moté <yvan@neopixl.com>
2019-10-22 19:17:22 +0200

    Modified Kotlin sample code

--- Android/Groupe 1/KotlinApp/app/src/main/java/com/neopixl/kotlinapp/MainActivity.kt
+++ Android/Groupe 1/KotlinApp/app/src/main/java/com/neopixl/kotlinapp/MainActivity.kt
@@ -3,6 +3,22 @@ package com.neopixl.kotlinapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Créer une function extension
+fun String.isPalindrome(): Boolean {
+    if(this.trim().isEmpty()) return false
+
+    val reversedWord = this.reversed()
+    return reversedWord.toLowerCase()==this.toLowerCase()
+}
+
+class Credential(val login: String, private val password: String, var validityDuration: Int) {
+
+    fun displayPassword() {
+        println("password: $password")
+    }
+
+}
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,5 +74,148 @@ class MainActivity : AppCompatActivity() {
         // En Kotlin on préfère utiliser
         // des tableaux avec le même type stocké (ex : que des String)
 
+
+        // MutableList (ajout, suppresion, …)
+        val brands = mutableListOf("HTC", "Samsung", "Xiaomi", "Huawei")
+
+        brands.add("One Plus")
+
+        brands.add(0, "Apple")
+
+        // Pour afficher un log dans la console
+
+        // [Apple, HTC, Samsung, Xiaomi, Huawei, One Plus]
+        println("brands : $brands")
+
+        // premier index = 0
+        brands.removeAt(0)
+
+        // Boucle
+
+        // for index
+
+        for(i in 0 until brands.size) {
+            val brandName = brands[i]
+
+            println("brand: $brandName")
+        }
+
+        // for each (Méthode conseillée)
+
+        for(brandName in brands) {
+            println("brand: $brandName")
+            val index = brands.indexOf(brandName)
+        }
+
+        // Don't worry
+        brands.forEachIndexed { index, brandName ->
+            println("brand at $index = $brandName")
+        }
+
+        // Tri
+
+        // Non destructive = récupération d'une copie du tableau triée
+
+        val sortedBrands = brands.sorted()
+
+        // Destructive = tri du tableau original
+
+        brands.sort()
+
+        // Map, filter, reduce
+
+        // Map = transformation d'un tableau avec un type vers un tableau avec un autre type
+        // List<Int> -> List<String>
+
+        val mapBrands = brands.map { brandName ->
+            "Marque $brandName"
+        }
+
+        // mapBrands = [Marque HTC, Marque Samsung, Marque Xiaomi, Marque Huawei, Marque One Plus]
+
+        // Filter = filter le tableau (le tableau retourné pas la méthode filter
+        // contient potentiellement moins d'éléments que le tableau d'origine)
+
+        val values = arrayOf(1, 2, 4, 5, 6, 7, 10)
+
+        // pour conserver les valeurs filtrées on doit retourner true
+        val evenValues = values.filter {
+            it%2==0
+        }
+
+        // evenValues = [2, 4, 6, 10]
+
+        // Reduce : A partir de plusieurs éléments on va récupérer un seul élément
+
+        // acc = valeur précédente accumulée
+        // value = valeur courante
+        val sum = values.reduce { acc, value ->
+            acc + value
+        }
+
+        // sum = 35
+
+        val sumWithInitialValue = values.fold(10, { acc, value ->
+            acc + value
+        })
+
+        // sumWithInitialValue = 45
+
+
+        // If
+        val isValid = true
+
+        if(isValid) {
+            println("C'est valide")
+        } else {
+            println("C'est invalide")
+        }
+
+        // Switch
+        // en Kotlin : when
+
+        val number = 3
+
+        when(number) {
+            1 -> println("Cas un")
+            2 -> println("Cas deux")
+            3 -> {
+                // Pour exécuter plusieurs lignes
+                println("Cas trois")
+            }
+            else -> println("Cas raté")
+
+        }
+
+        val testPalindrome = isPalindrome("Kayak")
+        val testPalindrome2 = isPalindrome("   ")
+
+        println("testPalindrome2: $testPalindrome2")
+
+        val testPalindrome3 = "Kayak".isPalindrome()
+
+        // Une Classe commence toujours pas une majuscule
+        // Un objet (une instance) ou un méthode commence toujours par une minuscule
+
+        val credential1 = Credential("admin@admin.com", "12345678", 12)
+        credential1.login //Ok
+        credential1.validityDuration // Ok
+        //credential1.password // Not ok
+
+        credential1.validityDuration = 300
+
+        credential1.displayPassword()
+
     }
+
+    // kayak = kayak
+    // radar = radar
+    // Kayak
+    fun isPalindrome(word: String): Boolean {
+        if(word.trim().isEmpty()) return false
+
+        val reversedWord = word.reversed()
+        return reversedWord.toLowerCase()==word.toLowerCase()
+    }
+
 }
--- Android/Groupe 2/KotlinApp/app/src/main/java/com/neopixl/kotlinapp/MainActivity.kt
+++ Android/Groupe 2/KotlinApp/app/src/main/java/com/neopixl/kotlinapp/MainActivity.kt
@@ -3,6 +3,24 @@ package com.neopixl.kotlinapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+class User(val firstName: String, val lastName: String,
+           val emailAddress: String, var age: Int) {
+
+    fun displayInformation() {
+        println("$firstName $lastName $emailAddress $age")
+    }
+}
+
+// "Kayak".isPalindrome()
+fun String.isPalindrome(): Boolean {
+    val reverseWord = this.reversed()
+
+    // kayak == kayak
+    // Kayak == kayaK
+    return this.toLowerCase() == reverseWord.toLowerCase()
+}
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,5 +72,151 @@ class MainActivity : AppCompatActivity() {
         val array2 = arrayOf("login", "password", "birthdate")
 
         // mutableList, filter, map, reduce
+
+        var values = mutableListOf(1, 4, 5, 6, 7)
+
+        // values = collection muable
+        values.add(8)
+
+        values.remove(5)    // return true
+
+        values.remove(99)   // return false
+
+        values.removeAt(0)    // suppresion du premier élément : index=0
+
+
+        // Boucler sur le tableau = parcourir tous les éléments
+
+        // For index de 0 à n
+
+        for(i in 0 until values.count()) {
+            val value = values[i]
+            println("valeur $value")
+        }
+
+        for(i in values.indices) {
+            val value = values[i]
+            println("valeur $value")
+        }
+
+        // For each
+
+        for(oneValue in values) {
+            println("valeur $oneValue")
+        }
+
+        // Tri du tableau
+
+        values.sort()       // Destructif, modifie le contenu
+
+        // Non destructif, récupération d'un nouveau tableau
+        val valuesSorted = values.sorted()
+
+        // Map, filter, reduce  -> Non destructif
+
+        // Map = transformation d'un tableau avec un type
+        // vers un tableau avec un autre type
+
+        // values : 4, 6, 7, 8
+
+        val mapValues = values.map { value ->
+            // value = 4
+            // value = 6
+            // value = 7
+            // value = 8
+
+            // Pour chaque valeur récupérée on retourne la valeur sous forme de String
+            value.toString()
+        }
+
+        // Filter = récupération d'un tableau filtré
+        // (avec potentiellement moins d'élements que l'original)
+
+        // it = valeur récupéré du tableau
+        val evenValues = values.filter {
+            // filter conserve la valeur du tableau si le retour == true
+            it%2==0
+        }
+
+        // evenValues = [4, 6, 8]
+
+        // Reduce = à partir d'un tableau on obtient une valeur
+
+        val sum = values.reduce { acc, i ->
+            // acc = valeur accumulé de la précédente itération
+            // i = nouvelle valeur (4, 6, 7 puis 8)
+            acc + i
+        }
+
+        println("sum $sum")  // 25
+
+        val isValid = true
+
+        if(isValid) {
+            println("C'est valide")
+        } else {
+            println("Ce n'est pas valide")
+        }
+
+        // switch
+
+        val number = 3
+
+        // when = switch
+        when(number) {
+            1 -> {
+                println("Cas un")
+            }
+            2 -> {
+                println("Cas deux")
+            }
+            3 -> {
+                println("Cas trois")
+            }
+            4 -> {
+                println("Cas quatre")
+            }
+            else -> {
+                println("Cas raté")
+            }
+        }
+
+        val palindromeTest = isPalindrome("Kayak") // true
+
+        val palindromeTest2 = "Kayak".isPalindrome()
+        "Radio".isPalindrome() // false
+
+        // création d'une instance (objet) User en appelant le constructeur
+        val user1 = User("John", "Weak",
+            "john.weak@hetic.net", 45)
+        user1.displayInformation()
+
+        // Une majuscule au début d'un Nom = Classe
+        // une minuscule au début d'un nom = variable / constante / nom_méthode
+
+        // Incorrect !!!
+        var Array1 = arrayOf(10, 2, 4)
+
+        // Correct
+        var array11 = arrayOf(10, 2, 4)
+
+        // user2 = instance (commence par une minuscule)
+        // User = classe (commence par une majuscule)
+        val user2 = User("Roxane", "Police",
+            "roxane@police.co.uk", 35)
+
+        user2.age++
+
     }
+
+    // word = kayak -> true
+    // word = boat -> false
+    fun isPalindrome(word: String): Boolean {
+        val reverseWord = word.reversed()
+
+        // kayak == kayak
+        // Kayak == kayaK
+        return word.toLowerCase() == reverseWord.toLowerCase()
+    }
+
 }
